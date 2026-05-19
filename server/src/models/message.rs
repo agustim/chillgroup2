853c7ee62a34ae2c34e82e6d2a948090a626539d
Agustí Moll <agusti@example.com>
2026-05-19 10:57:06 +0200

    feat(server): afegir sender_username al model Message

--- server/src/models/message.rs
+++ server/src/models/message.rs
@@ -10,6 +10,7 @@ pub struct Message {
     pub id: Uuid,
     pub channel_id: Uuid,
     pub sender_user_id: Uuid,
+    pub sender_username: Option<String>,
     pub sender_device_id: Uuid,
     pub encrypted_payload: String, // Base64 AES-GCM
     pub iv: String,                // Base64 nonce
